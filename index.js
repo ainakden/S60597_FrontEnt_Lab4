3122fd2964641710a469c8da73ea030e99b6ed93
ainakden <103589372+ainakden@users.noreply.github.com>
2022-04-21 22:17:58 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,39 +1,18 @@
-var colorTeal = "rgb(57, 204, 204)";
 $(function(){
+    var button = $("#button_change");
 
-    $("#button_toggle_colors").click(function() {
-        /* Step 2 */
-        
-        $(".box").each(function() {
-          if ($(this).css("background-color") == colorTeal) {
-            $(this).css("background-color", "");
-          } else {
-            $(this).css("background-color", colorTeal);
-          }
-        });
-      });
 
-      $("#button_toggle_roundedges").click(function() {
-        /*Step 3*/
-        $(".box").each(function() {
-          // if ($(this).hasClass("round-edge")) {
-          //   $(this).removeClass("round-edge");
-          // } else {
-          //   $(this).addClass("round-edge");
-          // }
-          $(this).toggleClass("round-edge");
-        });
-      });
+    $(button).click(function() {
+        // Actions to take upon button click goes here.
 
-      $("#button_add_box").click(function() {
-        /*Step 4 */
-        var new_box = document.createElement("div");
-        var boxCount = $(".box").length;
-        var new_id = boxCount + 1;
-        $(new_box).attr("id", "box" + new_id);
-        $(new_box).addClass("box");
-        $(new_box).addClass("outlined");
-        $("#boxes").append(new_box);
-      });
+        var inputValue = $("#input_color").val();
+   
 
+        var element = $(".cow_color");
+     
+     
+        $(element).html(inputValue);
+    });
+    
 })
+
